27dc6bccde59507c8b7d4e3e638d602b1f7dd325
soulch88888 <s.c.youg@gmail.com>
2023-11-26 01:16:20 +0200

    added the function for option 2: close tab

--- Soulayman Chaayto_FCS 50 Midterm.py
+++ Soulayman Chaayto_FCS 50 Midterm.py
@@ -1,8 +1,8 @@
 tabs_order = []   #this list will contain the keys of the opened tabs which will help us to keep track of their order.
-tab = {}          #this dictionary will contain the title, the url, the html content and the nested tabs(if there is any) for each opened tab.
+tabs= {}          #this dictionary will contain the title, the url, the html content and the nested tabs(if there is any) for each opened tab.
 
 
-def openTab(tabs_order,tab):
+def openTab(tabs_order,tabs):
   while True:             #while loop will break if the title is not empty
     title = input("Enter the title of the tab: ")
     if not title:
@@ -15,13 +15,28 @@ def openTab(tabs_order,tab):
       continue
     else:
       break
-  tab.update({title : url})        #add the title,url pair to the dictionary
+  tabs.update({title : url})        #add the title,url pair to the dictionary
   tabs_order.append(title)      #add the title to the end of list
 
 
+def closetab(tabs_order,tabs):
+  if len(tabs_order) > 0:
+    tabIndex = input("Enter the index of the tab to be removed: ")
+    if not tabIndex:                                #this condition is true if the input string is empty
+      print("deleted the tab with the title: ", tabs_order[-1])
+      tabs.popitem()        #this method removes the last added {key,value} pair from the dictionary
+      tabs_order.pop()
+    elif int(tabIndex) >= 0 and int(tabIndex) < len(tabs_order):
+      print("deleted the tab with the title: ", tabs_order[int(tabIndex)])
+      tabs.pop(tabs_order[int(tabIndex)])     #this method will take the key saved in the tabs_order list at the index provided by the user input and will remove the {key,value} pair from the dictionary
+      tabs_order.pop(int(tabIndex))        #this method will remove the title stored in the order list
+    else:
+      print("invalid input")
+  else:
+    print("There isn't any opened tabs yet.")
 
 
-def mainMenu(tabs_order,tab):
+def mainMenu(tabs_order,tabs):
   choice=-99 # dummy value
   while choice !=9:
     print("Welcome to the browser, please choose one of the options below:")
@@ -39,10 +54,10 @@ def mainMenu(tabs_order,tab):
 
     if choice == 1:
       print("Adding a new tab...")
-      openTab(tabs_order,tab)
+      openTab(tabs_order,tabs)
     elif choice == 2:
       print("Closing a tab...")
-      closetab()
+      closetab(tabs_order,tabs)
     elif choice == 3:
       print("Switching tabs...")
       switchTab()
@@ -66,7 +81,8 @@ def mainMenu(tabs_order,tab):
     else:
       print("invalid input")
 
-mainMenu(tabs_order,tab)
+mainMenu(tabs_order,tabs)
 #the last two lines are just for me, will be removed in future commits.
 print("tabs_order: ", tabs_order)
-print("tab: ", tab.items())
+print("tab: ", tabs.items())
+print(len(tabs_order))
