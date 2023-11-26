dc0f005eb209f0ac02eb75b50f566eef267b7b7b
soulch88888 <s.c.youg@gmail.com>
2023-11-26 20:03:33 +0200

    added the function for option 4: display all tabs

--- Soulayman Chaayto_FCS 50 Midterm.py
+++ Soulayman Chaayto_FCS 50 Midterm.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen  #this line allows us to use the urlopen() method (got this from google)
-tabs_order = []   #this list will contain the keys(titles) of the opened tabs which will help us to keep track of their order.
- #this dictionary will contain the title, the url, the html content and the nested tabs(if there is any) for each opened tab.
+tabs_order = []   #this list will contain the tabs(list of dictionaries) which will help us to keep track of their order.
+#the dictionary will contain the title, the url, the html content and the nested tabs(if there is any) for each opened tab.
 
 
 def openTab(tabs_order):
@@ -17,8 +17,8 @@ def openTab(tabs_order):
     else:
       break
   tab = {"title": title,
-          "url":  url}       #add the title,url pair to the dictionary
-  tabs_order.append(tab)      #add the title to the end of list
+          "url":  url}       #create a new dictionary that represents the tab
+  tabs_order.append(tab)      #add the tab to the end of list
 
 
 def closetab(tabs_order):
@@ -29,7 +29,7 @@ def closetab(tabs_order):
       tabs_order.pop()
     elif int(tabIndex) >= 0 and int(tabIndex) < len(tabs_order):
       print("deleted the tab with the title: ", tabs_order[int(tabIndex)]["title"])
-      tabs_order.pop(int(tabIndex))        #this method will remove the tab stored in the order list
+      tabs_order.pop(int(tabIndex))        #this method will remove the tab stored in the tabs_order list at the input index
     else:
       print("invalid input")
   else:
@@ -41,7 +41,7 @@ def switchTab(tabs_order):
     tabIndex = input("Enter the index of the tab you want to switch to: ")
     if not tabIndex:                                #this condition is true if the input string is empty
       print("Switching to the last opened tab with the title: ", tabs_order[-1]["title"])
-      url = tabs_order[-1]["url"]                         #this block of code is optained through google, and modified to fit my needs. we are taking the title of the last opened tab and the corresponding url from the dictionary,
+      url = tabs_order[-1]["url"]                         #this block of code is optained through google, and modified to fit my needs. we are taking the title of the last opened tab and the corresponding url from the list,
       page = urlopen(url)                                 #and then we are opening the url using urlopen() wich will return an HTTPResponse object, and saving it in the variable page.
       page_html_bytes = page.read()                       #After that we use the .read() method on page which will return the html content as a sequence of bytes, we will store it in page_html_bytes.
       html_content = page_html_bytes.decode("utf-8")      #Finally we are decoding the sequence of bytes to a string that represents the html content, and storing it in html_content
@@ -59,7 +59,22 @@ def switchTab(tabs_order):
     print("There isn't any opened tabs yet.")
 
 
-def sortAllTabs(list1):      #this function will sort the titles of the tabs saved in the order list alphabetically
+def displayAllTabs(tabs_order):
+  if len(tabs_order) > 0:
+    for tab in tabs_order:
+      print("the title of the tab: ", tab["title"])
+      if "nested tabs" in tab.keys():
+          print("nested tabs inside of ", tab["title"], " are: ", tab["nested tabs"]["title"])
+      else:
+          print("there is no nested tabs inside: ", tab["title"])
+  else:
+      print("There isn't any opened tabs yet.")
+
+
+
+
+
+def sortAllTabs(list1):      #this function will sort the tabs saved in the order list alphabetically by comparing their titles, using insertion sort.
   border = 1
   while border < len(list1):
     current = border
@@ -99,7 +114,7 @@ def mainMenu(tabs_order):
       switchTab(tabs_order)
     elif choice == 4:
       print("Displaying all tabs...")
-      displayAllTabs()
+      displayAllTabs(tabs_order)
     elif choice == 5:
       print("Opening nested tabs...")
       openNestedTabs()
